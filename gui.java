3242a5b1bb3c3d1b1c9ab6778f8211448bf4305d
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-19 14:23:57 +0530

    Added GUI in java

---
+++ gui.java
@@ -0,0 +1,13 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class gui extends JFrame
+{
+    
+}
--- main.java
+++ main.java
@@ -2,49 +2,6 @@ import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 public class main{
     public static void main(String[] args) {
-        // for (pikachu people : pikachu.values())
-        //     System.out.printf("%s\t%s\t%s\n", people,people.getDesc(),people.getage());
-        //     System.out.println();
-        //     for(pikachu people: EnumSet.range(pikachu.Gargi, pikachu.chutki))
-        //     System.out.printf("%s\t%s\t%s\n", people,people.getDesc(),people.getage());
-        //  Static1 pObject = new Static1("Raman ","Tank");   
-        //  Static1 pObject1 = new Static1("Rajat ","Tank");   
-        //  Static1 pObject2 = new Static1("Nitin ","Agarwal");   
-
-        //  System.out.println();
-        //  System.out.println(pObject.getFirst());
-        //  System.out.println(pObject.getSecond());
-        //  System.out.println(pObject.getMembers());
-        // final1 finalobj = new final1(7);
-        
-        // for (int i = 0; i < 5; i++) {
-        //         finalobj.add();
-        //     System.out.printf("%s", finalobj);
-        // }
-        // togipi objTogipi = new togipi();
-        // pikachu pikaobj = new pikachu();
-        // objTogipi.eat();
-        // pikaobj.eat();
-
-        // String fn = JOptionPane.showInputDialog("Enter a number 1");
-        // String sn = JOptionPane.showInputDialog("Enter a number 2");
-
-        // int num1 = Integer.parseInt(fn);
-        // int num2 = Integer.parseInt(sn);
-        // int sum = num1+num2;
-
-        // JOptionPane.showMessageDialog(null, "The addition of the numbers are " + sum, "Addition",JOptionPane.PLAIN_MESSAGE);
-        // pikachu raman = new pikachu();
-        // raman.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // raman.setSize(276,567);
-        // raman.setVisible(true);
-        // togipi raman = new togipi();
-        // raman.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // raman.setSize(256,256);
-        // raman.setVisible(true);
-        pokelist ll = new pokelist();
-        chari kk = new chari();
-        ll.add(kk);
-
+            
     }
 }
