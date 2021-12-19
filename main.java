03b26081e3d4e3579a6390e2fbc57a8dd93c861d
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-19 18:08:40 +0530

    Modified main function

--- main.java
+++ main.java
@@ -1,6 +1,7 @@
 import javax.swing.JOptionPane;
 import javax.swing.JFrame;
-public class main{
+public class main
+{
     public static void main(String[] args) 
     {
         Radio go = new Radio();
