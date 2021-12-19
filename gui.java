47b3d2d6f43581b27abd00af687b90e56820b257
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-19 15:21:15 +0530

    Added custom button

--- gui.java
+++ gui.java
@@ -1,5 +1,6 @@
 import java.awt.FlowLayout;
 import java.awt.event.ActionListener;
+import java.util.logging.Handler;
 import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -9,5 +10,35 @@ import javax.swing.JOptionPane;
 
 public class gui extends JFrame
 {
-    
+    private JButton reg;
+    private JButton custom;    
+
+    public gui()
+    {
+        super("I am doing GUI");
+        setLayout(new FlowLayout());
+
+        reg = new JButton("Reg Button");
+        add(reg);
+
+        Icon a = new ImageIcon(getClass().getResource("Goku.jpg")); 
+        Icon b = new ImageIcon(getClass().getResource("Pikachu.jpg"));
+        
+        custom = new JButton("Custom", a);
+        custom.setRolloverIcon(b);
+
+        add(custom);
+
+        HandlerClass handler = new HandlerClass(); 
+        reg.addActionListener(handler);
+        custom.addActionListener(handler);
+    }
+
+    private class HandlerClass implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            JOptionPane.showMessageDialog(null, String.format("%s", event.getActionCommand()));
+        }
+    }
 }
--- main.java
+++ main.java
@@ -1,7 +1,12 @@
 import javax.swing.JOptionPane;
 import javax.swing.JFrame;
 public class main{
-    public static void main(String[] args) {
-            
+    public static void main(String[] args) 
+    {
+        gui go = new gui();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300,200);
+        go.setVisible(true);
+
     }
 }
