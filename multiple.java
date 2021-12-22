efa1b3a103727dc8132495414fb7e2224ce76473
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-22 11:08:10 +0530

    Adding a Multile Selection List

---
+++ multiple.java
@@ -0,0 +1,38 @@
+import java.awt.event.ActionListener;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.logging.Handler;
+import java.awt.event.ActionEvent;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class multiple extends JFrame
+{
+    private JList leftlist;
+    private JList rightlist;
+    private JButton movebutton;
+    private static String[] foods = {"BURGER","PIZZA", "NITIN", "BOTTLE", "SPECS", "CAP", "JACKET", "NOSE", "CHAIN"};
+
+    public multiple()
+    {
+        super("This is a awesome title here ");
+        setLayout(new FlowLayout());
+
+        leftlist = new JList(foods);
+        leftlist.setVisibleRowCount(4);
+        leftlist.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        add(new JScrollPane(leftlist));
+
+        movebutton = new JButton("move --> ");
+        movebutton.addActionListener(
+            new ActionListener()
+            {
+                public void actionPerformed(ActionEvent event)
+                {
+                    rightlist.setListData(leftlist.getSelectedValues());
+                }
+            }
+        );
+        add(movebutton);
+    }
+}
