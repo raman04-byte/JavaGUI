0ce5d3a2c5cdaccf8d153cbe8ba5fb2cf1343361
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-22 08:55:59 +0530

    Added some functionality

--- Combo.java
+++ Combo.java
@@ -1,11 +1,7 @@
 import java.awt.event.ActionListener;
 import java.util.logging.Handler;
 import java.awt.event.ActionEvent;
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import javax.swing.JOptionPane;
+
 import javax.swing.text.html.ImageView;
 import java.awt.*;
 import java.awt.event.*;
@@ -13,12 +9,16 @@ import javax.swing.*;
 
 public class Combo extends JFrame {
 
-    private JCheckBox Box;
+    private JComboBox Box;
     private JLabel picture;
 
     private static String[] filename = {"Button.jpg","Goku.jpg"};
     private Icon[] pics = {new ImageIcon(getClass().getResource(filename[0]))}; 
     public Combo() {
         super("Title here");
+        setLayout(new FlowLayout());
+
+        Box = new JComboBox(filename);
+        
     }
 }
