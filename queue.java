e078a18bc870123cb3666b680f95b64a679992ed
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-26 14:26:56 +0530

    Added queue

---
+++ queue.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class queue 
+{
+    public static void main(String[] args) {
+        PriorityQueue<String> q = new PriorityQueue<String>();
+        q.offer("first");
+        q.offer("second");
+        q.offer("third");
+
+        System.out.printf("%s ",q);
+        System.out.println();
+        
+        System.out.printf("%s",q.peek());
+        System.out.println();
+        q.poll();
+        System.out.printf("%s ",q);
+
+    }    
+}
