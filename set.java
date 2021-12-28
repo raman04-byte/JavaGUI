ff1eee7659f95f5999e06caffd333d9ee82ee0a0
raman-04 byte <2020pgicsraman65@poornima.org>
2021-12-28 19:01:05 +0530

    Added hash set code

---
+++ set.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class set 
+{
+    public static void main(String[] args)
+    {
+        // String[] things = {"Raman","raman","Raman","Tank"};
+        String[] things = {"Raman","tt","Raman","Tank"};
+        List<String> list = Arrays.asList(things);
+        System.out.println("The values in the string are ");
+        System.out.printf("%s ", list);
+        System.out.println();
+        Set<String> tt = new HashSet<String>(list);
+        System.out.printf("%s ", tt);
+        System.out.println();
+    }    
+}
