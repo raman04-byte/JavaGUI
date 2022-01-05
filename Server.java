979813f4cd493fb1fbfaafde931f3f6891b0206b
raman-04 byte <2020pgicsraman65@poornima.org>
2022-01-05 22:41:13 +0530

    Added server side java

--- Server.java
+++ Server.java
@@ -3,6 +3,7 @@ import java.net.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class Server extends JFrame
 {
@@ -11,12 +12,126 @@ public class Server extends JFrame
     private ObjectOutputStream output;
     private ObjectInputStream input;
     private ServerSocket server;
-    private Socket connections;
+    private Socket connection;
     
     // Constructor
     public Server()
     {
         super("Raman Instant Message");
         userText = new JTextField();
+        userText.setEditable(false);
+        userText.addActionListener(
+            new ActionListener()
+            {
+                public void actionPerformed(ActionEvent event)
+                {
+                    sendMessage(event.getActionCommand());
+                    userText.setText("");
+                }
+            }
+        );
+        add(userText, BorderLayout.NORTH);
+        chatWindow = new JTextArea();
+        add(new JScrollPane(chatWindow));
+        setSize(300,150);
+        setVisible(true);
+    }
+
+    private void startrunning() 
+    {
+        try{
+            server = new ServerSocket(6789,100);
+            while(true)
+            {
+                try {
+                    waitForConnection();
+                    setUpStreams();
+                    whileChatting();
+                } catch (EOFException eofexception) {
+                    showMessage("\n Connections has been ended see you soon !");
+                }finally
+                {
+                    closeCrap();
+                }
+            }
+        }   
+        catch(IOException ioexception)
+        {
+            ioexception.printStackTrace();
+        } 
+    }
+    // Waiting for connections public method is being used
+    private void waitForConnection() throws IOException 
+    {
+        showMessage("Waiting for someone to connect !! ... \n");
+        connection = server.accept(); 
+        showMessage("Now connected to someone with address " + connection.getInetAddress().getHostName());
+    }
+    // Stream for send and receive data 
+    private void setUpStreams() throws IOException
+    {
+        output = new ObjectOutputStream(connection.getOutputStream());
+        output.flush();
+        input = new ObjectInputStream(connection.getInputStream());
+        showMessage("\n Stream is now set up !!");    
+    }
+    // During the chat conversation 
+    private void whileChatting() throws IOException{
+            String message = "You are now connected for the chat ";
+            sendMessage(message);
+            ableToType(true);
+            do {
+                try {
+                    message = (String)input.readObject();
+                    showMessage("\n"+ message);
+                } catch (ClassNotFoundException classNotFoundException) {
+                     showMessage("User send this message");
+                }
+            } while (!message.equals("CLIENT- END"));
+    }
+    // Close streams and sockets after chatting 
+    private void closeCrap()
+    {
+        showMessage("\nCloseing connections...\n");
+        ableToType(false);
+        try {
+            output.close();
+            input.close();
+            connection.close();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+    // Send message to someone using some java and some techniquus
+    private void sendMessage(String message)
+    {
+        try {
+            output.writeObject("SERVER " + message);
+            output.flush();
+            showMessage("SERVER " + message);
+        } catch (IOException ioException) {
+            chatWindow.append("\n ERROR: I CAN'T SEND ANY MESSAGE");
+        }
+    }
+    // Updating chatwindow
+    private void showMessage(final String text)
+    {
+        SwingUtilities.invokeLater(
+            new Runnable() {
+                public void run() {
+                    chatWindow.append(text);
+                }
+            }
+        );
+    }  
+    // Letting user to type stuff into the box
+    private void ableToType(final boolean tof) {
+        SwingUtilities.invokeLater(
+            new Runnable() {
+             public void run() {
+                 userText.setEditable(tof);
+             }   
+            }
+        );
     }
 }
--- main.java
+++ main.java
@@ -4,8 +4,10 @@ public class main
 {
     public static void main(String[] args) 
 {
-    ReadFile dude = new ReadFile();
-    dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    
+    // ReadFile dude = new ReadFile();
+    // dude.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    Server me = new Server();
+    me.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // me.startRunning();
 }
 }
