af49e6069e46464a48b60810eef0223a2ba1099c
raman-04 byte <2020pgicsraman65@poornima.org>
2022-01-09 13:19:49 +0530

    Completed screen manager and modified screen manager

--- raman.java
+++ raman.java
@@ -8,63 +8,70 @@ public class raman extends JFrame {
         raman r = new raman();
         r.run(displayMode);
     }
-    
-    private Screen screen;
-    private Image bg;
     private Animation a;
+    private ScreenManager s;
+    private Image bg;
+    
+    private static final DisplayMode modes1[] = {
+        new DisplayMode(800,600,32,0),
+        new DisplayMode(800,600,24,0),
+        new DisplayMode(800,600,16,0),
+        new DisplayMode(640,480,32,0),
+        new DisplayMode(640,480,24,0),
+        new DisplayMode(640,480,16,0),
+    };
 
-    public void loadpics() {
-        bg = new ImageIcon("Goku.jpg").getImage();
-        Image face1 = new ImageIcon("Pikachu.jpg").getImage();
-        Image face2 = new ImageIcon("Pikachu1.jpg").getImage();
-        Image face3 = new ImageIcon("Pikachu2.png").getImage();
-        Image face4 = new ImageIcon("Pikachu3.png").getImage();
-        Image face5 = new ImageIcon("Pikachu4.jpg").getImage();
-        Image face6 = new ImageIcon("Pikachu5.png").getImage();
+    public void loadImages(){
+        bg = new ImageIcon().getImage();
+        
+    Image face1 = new ImageIcon("Pikachu.jpg").getImage();
+    Image face2 = new ImageIcon("Pikachu1.jpg").getImage();
+    Image face3 = new ImageIcon("Pikachu2.png").getImage();
+    Image face4 = new ImageIcon("Pikachu3.png").getImage();
+    Image face5 = new ImageIcon("Pikachu4.jpg").getImage();
+    Image face6 = new ImageIcon("Pikachu5.png").getImage();
 
-        a = new Animation();
+    a = new Animation();
         a.addscene(face1, 250);
         a.addscene(face2, 250);
         a.addscene(face3, 250);
         a.addscene(face4, 250);
         a.addscene(face5, 250);
         a.addscene(face6, 250);
-     }
-     public void run(DisplayMode dm)
-     {
-        screen = new Screen();
-        try {
-            screen.setFullScreen(dm, new JFrame());
-            loadpics();
-            movieLoop();
+    }
+    public void run(DisplayMode displayMode){
+        s = new ScreenManager();
+        try{
+            DisplayMode dm = s.findFirstCompatablMode(modes1);
+            s.setFullScreen(dm);
+            loadImages();
+            movieloop();
         }finally{
-            screen.restoreScreen();
+            s.restoreScreen();
         }
-     }
-     public void movieLoop()
-     {
-         long startingTime = System.currentTimeMillis();
-         long cumtime = startingTime;
+    }
+    public void movieloop(){
+        long startingTime = System.currentTimeMillis();
+        long cumTime = startingTime;
+        while(cumTime - startingTime < 6000){
+            long timepassed = System.currentTimeMillis() - cumTime;
+            cumTime += timepassed;
+            a.update(timepassed);
+            
+            // Drawing and updating screen
+            Graphics2D g = s.getGraphics();
+            draw(g);
+            g.dispose();
+            s.update();
+            try
+            {
+                Thread.sleep(20);
+            }catch(Exception ex){}
+        }
 
-         while(cumtime - startingTime < 5000)
-         {
-             long timepassed = System.currentTimeMillis() - cumtime;
-             cumtime += timepassed;
-             a.update(timepassed);
-
-             Graphics g = screen.getFullScreeWindow().getGraphics();
-             draw(g);
-             g.dispose();
-
-             try{
-                 Thread.sleep(20);
-             }
-             catch(Exception ex){}
-         }
-     }
-     public void draw(Graphics g)
-     {
-         g.drawImage(bg, 0, 0, null);
-         g.drawImage(a.getImage(), 0, 0, null);
- 
-}}
+    }
+    public void draw (Graphics g){
+        g.drawImage(bg, 0, 0, null);
+        g.drawImage(a.getImage(), 0, 0, null);
+    }
+}
