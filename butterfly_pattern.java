afa43692b0c9f80d70f952d6a0ef49613b1b01a9
raman-04 byte <2020pgicsraman65@poornima.org>
2022-01-26 18:47:50 +0530

    Added pyramidf

--- butterfly_pattern.java
+++ butterfly_pattern.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class butterfly_pattern {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 9999;
         // First half
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
